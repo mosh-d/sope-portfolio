b4498889cc93dc0f0c5ce7785d92ec67cd44efac
mosh-d <shopsy8@gmail.com>
2025-12-20 15:53:49 +0100

    Adding App data

---
+++ src/utils/data.js
@@ -0,0 +1,193 @@
+const sope = [
+  {
+    role: "designer",
+    tags: [
+      "UI/UX",
+      "Info Architecture",
+      "Style Sheets",
+      "Wireframes",
+      "Mockups",
+      "Prototypes",
+      "Case Studies",
+    ],
+    title: "UI/UX Designer",
+    about:
+      "I’m a highly creative UX designer with a strong background in art, animation, and front-end development. Deep understanding of UI/UX design concepts leads to a strong passion for designing user-centered solutions that are interactive, functional, accessible, and user-friendly. Proficient in front-end development using HTML, CSS, and JavaScript, which can aid a smooth and efficient transition from design to development. I also bring a business lens to my work. With a Masters degree in Project Management, I understand how to balance stakeholder expectations with design and business requirements—navigating time, cost, and quality constraints to deliver real value. I’m passionate about Japanese culture. I enjoy Japanese fashion, TV shows, anime, and karate. I speak 5 languages: Design, Engineering, Business, English, and Japanese.",
+    experience: [
+      {
+        duration: "Jun 2024 - Present",
+        title: "UX Designer at FriendnPal (volunteer)",
+        descriptionPoints: [
+          "Designed low fidelity wireframes for the FriendnPal version 2 mental fitness app. Utilized user flows, Information architecture, crazy 8's, storyboards and other techniques to narrow down on key features and navigation. Reduced the number of screens and sped up the time needed to complete key tasks by an average of 17 seconds from the version 1.",
+          "Utilized design thinking and experimented with unique layouts and styles to optimize screen space for maximum efficiency.",
+          "Converted the lo-fi wireframes to high fidelity mockups and prototypes ready for development. Designed reusable components.",
+          "Maintained cohesion with the development team. At certain points, review meetings with the development team weren’t necessary as I was able to provide insights on technical feasibility and constraints.",
+          "Constantly making necessary updates to the app design based on added requirements from stakeholders.",
+        ],
+        tags: ["Wireframes", "Mockups", "Prototypes", "Figma"],
+      },
+      {
+        duration: "May 2025 - Present",
+        title: "Lead Software Engineer at Five Clover",
+        descriptionPoints: [
+          "Audited the old company website to pinpoint focus areas that needed to be improved (including layout, color usage, typography, image loading, and other performance metrics) and designed mockups for a new, modern, dynamic website for the Five Clover hotel management company.",
+          "Developed mockups into a high-fidelity prototype for the Five Clover website.",
+          "Presented the new design to key stakeholders, got feedback, and implemented corrections where necessary.",
+          "Developed the new hotel website frontends using React and tailwind CSS for fast, component-based Single Page Applications (SPAs) and established a strong CSS codebase accommodating the robust and unique design system of the websites and brand.",
+          "Designed the backend architecture and hosting structure to accommodate the various websites that would be built for each hotel branch.",
+          "Setup a multi-tenant postgreSQL database and a robust backend API to handle room inventory and reservation logic based on business requirements (a single backend API serving multiple hotel branch frontends based on branch IDs).",
+        ],
+        tags: [
+          "User Research",
+          "Info Architecture",
+          "Wireframes",
+          "Mockups",
+          "Prototypes",
+          "Figma",
+          "CSS",
+          "React",
+          "tailwind",
+          "PostgreSQL",
+          "Node.js",
+          "Express.js",
+        ],
+      },
+      {
+        duration: "Jun 2024 - Sep 2025",
+        title:
+          "Lead Front-end Engineer at Totesphere (Part-time from May 2025)",
+        descriptionPoints: [
+          "Designed a high-fidelity, dynamic, and intuitive prototype for the Totesphere website (a website dedicated to providing grants for startup businesses) using Figma. Focused on maintaining brand identity while crafting interactive components that fell in line with color theory and design principles.",
+          "Continued to bring the website design to life with HTML, CSS, and JavaScript. Added dynamic components with the aid of event listeners, url manipulation, and other intuitive functions and methods.",
+          "Implemented API request for Google map data.",
+          "Optimized page loading, leading to an average load time of 2 seconds, down 50% from 4 seconds.",
+        ],
+        tags: ["Mockups", "Prototypes", "Figma", "CSS", "SCSS", "JavaScript"],
+      },
+    ],
+    skills: [
+      {
+        title: "Figma",
+        tags: [
+          "User Personas",
+          "Empathy Maps",
+          "Site Maps",
+          "User Flows",
+          "Style Sheets",
+          "Design Systems",
+          "HiFi Wireframes",
+          "Mockups",
+          "Prototypes",
+        ],
+      },
+      {
+        title: "Clip Studio Paint",
+        tags: [
+          "Idea Drafts",
+          "Sketches",
+          "Art",
+          "GIFs",
+          "Storyboards",
+          "LoFi Wireframes",
+        ],
+      },
+      {
+        title: "Google",
+        tags: ["Google Forms", "Questionnaires", "Information Architecture"],
+      },
+      {
+        title: "Mental",
+        tags: [
+          "User Research",
+          "User Centered Design",
+          "Accessibility",
+          "Usability",
+          "Functionality",
+          "Design Thinking",
+          "Gestalt Principles",
+          "Composition",
+          "Color Theory",
+        ],
+      },
+    ],
+    projects: [
+      "The Cordis Hotel Web Design",
+      "Five Clover Hotels Group Website",
+      "Totesphere Official Website Mockups",
+      "Console Game Booking App Case Study",
+      "Gaming Lounge Banner Design",
+      "Fashion E-commerce Website Landing Page",
+      "Motion Graphics",
+      "Digital Art",
+    ],
+  },
+  {
+    role: "engineer",
+    tags: {
+      frontEnd: ["HTML", "CSS", "JavaScript", "React", "Next.js"],
+      backEnd: ["Node.js", "Express.js", "MongoDB", "PostgreSQL"],
+    },
+    title: "Full-stack Software Engineer",
+    about:
+      "While I can architect server-side logic and manage databases, my true strength lies in crafting interfaces that users remember and love to use.I also bring a business lens to my work. With a Masters degree in Project Management, I understand how to balance stakeholder expectations with technical and business requirements—navigating time, cost, and quality constraints to deliver real value. I’m passionate about Japanese culture. I enjoy Japanese fashion, TV shows, anime, and karate. I speak 5 languages: Design, Engineering, Business, English, and Japanese.",
+    experience: [
+      {
+        duration: "May 2025 - Present",
+        title: "Lead Software Engineer at Five Clover",
+        descriptionPoints: [
+          "Audited the old company website to pinpoint focus areas that needed to be improved (including layout, color usage, typography, image loading, and other performance metrics) and designed mockups for a new, modern, dynamic website for the Five Clover hotel management company.",
+          "Developed mockups into a high-fidelity prototype for the Five Clover website.",
+          "Presented the new design to key stakeholders, got feedback, and implemented corrections where necessary.",
+          "Developed the new hotel website frontends using React and tailwind CSS for fast, component-based Single Page Applications (SPAs) and established a strong CSS codebase accommodating the robust and unique design system of the websites and brand.",
+          "Designed the backend architecture and hosting structure to accommodate the various websites that would be built for each hotel branch.",
+          "Setup a multi-tenant postgreSQL database and a robust backend API to handle room inventory and reservation logic based on business requirements (a single backend API serving multiple hotel branch frontends based on branch IDs).",
+        ],
+        tags: [
+          "CSS",
+          "React",
+          "tailwind",
+          "PostgreSQL",
+          "Node.js",
+          "Express.js",
+        ],
+      },
+      {
+        duration: "Sep 2022 - Jun 2023",
+        title: "Software Engineer Trainee at ALX Africa",
+        descriptionPoints: [
+          "Learned and applied various skills including Git bash, Linux Shell and C programming language to complete coding tasks.",
+        ],
+        tags: ["Git", "C", "bash", "devOps", "Linux OS"],
+      },
+      {
+        duration: "Mar 2020 - Oct 2020",
+        title: "Web Developer Intern at MClord’s Mobile Solutions",
+        descriptionPoints: [
+          "Designed and developed a responsive website for a local restaurant by leveraging HTML and CSS to create a visually appealing and user-friendly web interface. Ensured seamless functionality across various devices, including desktops, tablets, and mobile phones. Collaborated with cross-functional teams",
+        ],
+        tags: ["HTML", "CSS"],
+      },
+    ],
+    skills: [
+      "React",
+      "Next.js",
+      "Node.js",
+      "Express.js",
+      "PostgreSQL",
+      "MongoDB",
+      "JavaScript",
+      "CSS",
+      "Tailwind",
+      "SCSS",
+      "Git",
+      "HTML",
+    ],
+    projects: [
+      "Cordis Hotel Website (front end)",
+      "Five Clover Hotel Website (full stack)",
+      "Totesphere Partnerships Website (front end)",
+    ],
+  },
+];
+
+export default sope;

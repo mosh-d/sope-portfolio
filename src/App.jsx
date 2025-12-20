d1bdce1fb69d109e1180b181d366b5698a6bb872
mosh-d <shopsy8@gmail.com>
2025-12-20 14:46:45 +0100

    Configuring App-wide  state switching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [ role, setRole ] = useState("");
+
+  const dataIndex = role === "designer" ? 0 : role === "engineer" ? 1 : undefined;
+
   return (
     <>
-      Hello World!
+      <header className="text-[color:var(--black-color)]/50 bg-[color:var(--white-color)] h-[40rem]">
+        <p>HERO SECTION</p>
+        <div className="flex h-[100%]">
+          <section onClick={setRole("designer")} className="w-[50%] h-[100%] cursor-pointer">
+            <h2>designer</h2>
+          </section>
+          <section onClick={setRole("engineer")} className="w-[50%] cursor-pointer">
+            <h2>engineer</h2>
+          </section>
+        </div>
+      </header>
+      <main>Hello World!</main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
